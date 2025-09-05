89e03b6eec59fdfdfa8a17d5395d89a6364c8981
Ubuntu <ubuntu@ip-10-24-1-66.ap-southeast-3.compute.internal>
2025-09-05 06:14:06 +0000

    version with oss POD Identity Token

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	aliyuncredentials "github.com/aliyun/credentials-go/credentials"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/gorilla/mux"
 )
@@ -15,6 +20,12 @@ import (
 var (
 	s3Client   *s3.Client
 	bucketName string
+)
+
+const (
+	EnvRoleArn         = "ALIBABA_CLOUD_ROLE_ARN"
+	EnvOidcProviderArn = "ALIBABA_CLOUD_OIDC_PROVIDER_ARN"
+	EnvOidcTokenFile   = "ALIBABA_CLOUD_OIDC_TOKEN_FILE"
 )
 
 func listFilesHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,9 +49,109 @@ func listFilesHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(files)
 }
 
+func newOidcCredential() (aliyuncredentials.Credential, error) {
+	// https://www.alibabacloud.com/help/doc-detail/378661.html
+	aliyunconfig := new(aliyuncredentials.Config).
+		SetType("oidc_role_arn").
+		SetRoleArn(os.Getenv(EnvRoleArn)).
+		SetOIDCProviderArn(os.Getenv(EnvOidcProviderArn)).
+		SetOIDCTokenFilePath(os.Getenv(EnvOidcTokenFile)).
+		SetRoleSessionName("test-rrsa-oidc-token").
+		SetRoleSessionExpiration(3600).    // We set the expiration time of the role session to 3600 seconds (1 hour).
+		SetSTSEndpoint("sts.aliyuncs.com") // https://next.api.aliyun.com/product/Sts
+
+	fmt.Println("Executing this credential using OIDC")
+	oidcCredential, err := aliyuncredentials.NewCredential(aliyunconfig)
+
+	return oidcCredential, err
+}
+
+type Credentials struct {
+	AccessKeyId     string
+	AccessKeySecret string
+	SecurityToken   string
+}
+
+func (credentials *Credentials) GetAccessKeyID() string {
+	return credentials.AccessKeyId
+}
+
+func (credentials *Credentials) GetAccessKeySecret() string {
+	return credentials.AccessKeySecret
+}
+
+func (credentials *Credentials) GetSecurityToken() string {
+	return credentials.SecurityToken
+}
+
+func NewEnvironmentVariableCredentials() (*Credentials, error) {
+	var envCredential *Credentials
+
+	oidcCredential, err := newOidcCredential()
+	if err != nil {
+		panic(err)
+	}
+	credentialAliyun, err := oidcCredential.GetCredential()
+
+	accessID := *credentialAliyun.AccessKeyId
+	if accessID == "" {
+		return envCredential, fmt.Errorf("access key id is empty!")
+	}
+	accessKey := *credentialAliyun.AccessKeySecret
+	if accessKey == "" {
+		return envCredential, fmt.Errorf("access key secret is empty!")
+	}
+	token := *credentialAliyun.SecurityToken
+	envCredential = &Credentials{
+		AccessKeyId:     accessID,
+		AccessKeySecret: accessKey,
+		SecurityToken:   token,
+	}
+
+	return envCredential, nil
+}
+
+func NewAwsS3Provider(credential *Credentials) credentials.StaticCredentialsProvider {
+
+	return credentials.StaticCredentialsProvider{
+		Value: aws.Credentials{
+			AccessKeyID:     credential.AccessKeyId,
+			SecretAccessKey: credential.AccessKeySecret,
+			SessionToken:    credential.SecurityToken,
+		},
+	}
+}
+
 func main() {
+	// Initialize credentials and OSS Comptatible S3 client
+	region := os.Getenv("OSS_REGION")
+	endpoint := fmt.Sprintf("https://oss-%s.aliyuncs.com", region)
+
+	// Define a custom endpoint resolver
+	customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+		return aws.Endpoint{
+			PartitionID:   "oss",
+			URL:           endpoint,
+			SigningRegion: "us-east-1",
+		}, nil
+	})
+
+	// Fetch credentials from environment variables
+	envCredential, err := NewEnvironmentVariableCredentials()
+	if err != nil {
+		log.Printf("error: %v", err)
+		return
+	}
+
+	// Provider to using OIDC token
+	provider := NewAwsS3Provider(envCredential)
+
 	// Load AWS config from env variables or shared config
-	cfg, err := config.LoadDefaultConfig(context.TODO())
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("us-east-1"),
+		config.WithCredentialsProvider(provider),
+		config.WithEndpointResolverWithOptions(customResolver),
+	)
 	if err != nil {
 		log.Fatal("Unable to load AWS SDK config:", err)
 	}
@@ -60,3 +171,4 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
